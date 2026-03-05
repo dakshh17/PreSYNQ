6ed503413f7dd10c0f47fbc64b40359fdbf6b2ff
dakshh17 <mistrydaksh0610@gmail.com>
2026-03-05 14:53:53 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function startClass(){
+document.getElementById("qr").innerText="QR SESSION ACTIVE";
+}
+
+function markAttendance(){
+setTimeout(()=>{
+alert("Attendance Successfully Marked");
+},1000);
+}
